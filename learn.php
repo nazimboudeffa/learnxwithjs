3f5e1d6dafab4f62d4e48cd099fdf1a0bc1edd1e
Nazim Boudeffa <boudeffa@gmail.com>
2023-07-21 18:20:43 +0200

    Create learn.php

---
+++ learn.php
@@ -0,0 +1,7 @@
+// Single-line comments start with two slashes.
+/* Multiline comments start with slash-star,
+   and end with star-slash */
+
+// Statements must be terminated by ;
+doStuff();
+do_stuff();
